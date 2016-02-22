54c26711d44bac9ca37806e2e9ed5e813483db11
oguzhansagoglu <alwaysprep@gmail.com>
2016-02-22 10:23:49 +0200

    expected fail LogisticRegressionModel example

---
+++ examples/LogisticRegressionModel.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+
+from npml.LogisticRegression import LogisticRegressionModel
+
+iris = datasets.load_iris()
+
+X = iris.data[0:100, :2]
+Y = iris["target"][0:100]
+
+logreg = LogisticRegressionModel()
+
+logreg.fit(X, Y)
+
+
+h = .02 
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
+
+Z = Z.reshape(xx.shape)
+plt.figure(1, figsize=(4, 3))
+plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+
+
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=Y, edgecolors='k', cmap=plt.cm.Paired)
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
