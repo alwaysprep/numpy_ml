13fc124d38a3fe6008db5566956c8f5b1eadc8f0
oguzhansagoglu <alwaysprep@gmail.com>
2016-03-08 12:24:00 +0200

    shuffle data before split to train and test

--- examples/LogisticRegressionModel.py
+++ examples/LogisticRegressionModel.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+
 
 from npml.LogisticRegression import LogisticRegressionModel
 
 iris = datasets.load_iris()
 
-X = iris.data[0:100, :2]
-Y = iris["target"][0:100]
+X, x, Y, y = train_test_split(iris.data[0:100, :2], iris["target"][0:100], test_size=0.20)
+
 
 logreg = LogisticRegressionModel()
 
 logreg.fit(X, Y)
-
 
 h = .02 
 x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
@@ -26,7 +27,7 @@ plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
 
 
 # Plot also the training points
-plt.scatter(X[:, 0], X[:, 1], c=Y, edgecolors='k', cmap=plt.cm.Paired)
+plt.scatter(x[:, 0], x[:, 1], c=y, edgecolors='k', cmap=plt.cm.Paired)
 plt.xlabel('Sepal length')
 plt.ylabel('Sepal width')
 
